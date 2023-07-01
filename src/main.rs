22170e940ac0e070f85f900e0c7b2ac2ed6d3d3d
julian stremel <74060086+JulianStremel@users.noreply.github.com>
2023-07-01 17:49:28 +0200

    basic parser using pest is working | parsing works from commandline or from file using flag -f

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,20 @@ extern crate pest;
 
 #[macro_use]
 extern crate pest_derive;
-use pest::Parser;
 
+use pest::Parser;
+use std::{fs,env};
 
 #[derive(Parser)]
 #[grammar = "grammar.pest"]
-struct IdentParser;
+struct ProgramParser;
 
 
 
 fn parse(input:String) {
-    let pairs = IdentParser::parse(Rule::program, &input).unwrap_or_else(|e| panic!("{}", e));
+    let pairs = ProgramParser::parse(Rule::program, &input).unwrap_or_else(|e| panic!("{}", e));
 
-    // Because ident_list is silent, the iterator will contain idents
+    // 
     for pair in pairs {
         // A pair is a combination of the rule which matched and a span of input
         println!("Rule:    {:?}", pair.as_rule());
@@ -26,18 +27,38 @@ fn parse(input:String) {
             match inner_pair.as_rule() {
                 Rule::COMMENT => println!("Comment:  {}", inner_pair.as_str()),
                 Rule::expression => println!("Expression:   {}", inner_pair.as_str()),
+                Rule::functionCall => println!("Functioncall:   {}", inner_pair.as_str()),
+                Rule::EOI => println!("reached EOF"),
                 _ => unreachable!()
             };
         }
     }
 }
 
+fn direct_mode(input:String) {
+    println!("will parse script as raw input");
+    parse(input);
+}
+
+fn file_mode(input:&Vec<String>){
+    let file = fs::read_to_string(&input[2]).unwrap_or_else(|e| panic!("{}", e) );
+    println!("debug{:?}",file);
+    parse(file);
+}
+
 fn main() {
-    let example:String = "// hi \nwhats = test()\r".to_string();
+
     // 1. call program as `lime <script>`
-    let args = std::env::args();
-    println!("{:?}",args);
-    parse(example);
+    let args: Vec<String> = env::args().collect();
+    let arg_len = args.len();
+    match arg_len {
+        1 => println!("please specify input or get help using -h"),
+        2 => direct_mode(args[0].to_string()),
+        3 => file_mode(&args),
+        _ => println!("wtf is your problem ?"),
+    }
+
+    //parse(example);
     
     // 2. load script as lines
 
