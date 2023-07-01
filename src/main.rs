9dc580d1ee1bb4f262433b699f427e25adf00ed6
julian stremel <74060086+JulianStremel@users.noreply.github.com>
2023-07-01 16:39:53 +0200

    moved lexer into main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,43 @@
-mod lexer;
+extern crate pest;
 
+#[macro_use]
+extern crate pest_derive;
+use pest::Parser;
+
+
+#[derive(Parser)]
+#[grammar = "grammar.pest"]
+struct IdentParser;
+
+
+
+fn parse(input:String) {
+    let pairs = IdentParser::parse(Rule::program, &input).unwrap_or_else(|e| panic!("{}", e));
+
+    // Because ident_list is silent, the iterator will contain idents
+    for pair in pairs {
+        // A pair is a combination of the rule which matched and a span of input
+        println!("Rule:    {:?}", pair.as_rule());
+        println!("Span:    {:?}", pair.as_span());
+        println!("Text:    {}", pair.as_str());
+
+        // A pair can be converted to an iterator of the tokens which make it up:
+        for inner_pair in pair.into_inner() {
+            match inner_pair.as_rule() {
+                Rule::COMMENT => println!("Comment:  {}", inner_pair.as_str()),
+                Rule::expression => println!("Expression:   {}", inner_pair.as_str()),
+                _ => unreachable!()
+            };
+        }
+    }
+}
 
 fn main() {
+    let example:String = "// hi \nwhats = test()\r".to_string();
     // 1. call program as `lime <script>`
     let args = std::env::args();
     println!("{:?}",args);
+    parse(example);
     
     // 2. load script as lines
 
