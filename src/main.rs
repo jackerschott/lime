9a526fb7e3322bda3beb68a348e843c1066224ca
JulianStremel <74060086+JulianStremel@users.noreply.github.com>
2023-07-02 10:54:00 +0000

    parser now just parses and nothin else -> this breaks functionalitiy for now!

--- src/main.rs
+++ src/main.rs
@@ -3,74 +3,100 @@ extern crate pest;
 #[macro_use]
 extern crate pest_derive;
 
-use pest::Parser;
-use std::{fs,env, vec};
+use pest::{Parser, iterators::{Pairs, Pair}};
+use std::{fs,env, vec, clone};
 
 #[derive(Parser)]
 #[grammar = "grammar.pest"]
 struct ProgramParser;
 
+const FLAGS: &'static [&'static str] = &["-f", "-h","-s"];
+
+const HELPTEXT: &'static str = "lime - a simple image manipulation language\n
+    usage: lime [flag] <data>\n
+    -f <file>   parse script from file\n
+    -s <script> parse script from commandline\n
+    -h          print this help text\n";
+
+// enum for commandline arguments and their values
 #[derive(Debug)]
-struct CmdArgument{
-    flag:String,
-    value:String
+enum Flag {
+    F(String),
+    S(String),
+    H,
+    D,
 }
 
+// parse input string using pest grammar
+fn parse(input:&String) -> Vec<Pair<'_, Rule>> {
+    let pairs:Pairs<'_, Rule> = ProgramParser::parse(Rule::program, &input).unwrap_or_else(|e| panic!("{}", e));
+    pairs.collect::<Vec<Pair<'_, Rule>>>()
+}
 
-fn parse(input:String) {
-    let pairs = ProgramParser::parse(Rule::program, &input).unwrap_or_else(|e| panic!("{}", e));
-
-    // 
-    for pair in pairs {
-        // A pair is a combination of the rule which matched and a span of input
-        println!("Rule:    {:?}", pair.as_rule());
-        println!("Span:    {:?}", pair.as_span());
-        println!("Text:    {}", pair.as_str());
-
-        // A pair can be converted to an iterator of the tokens which make it up:
-        for inner_pair in pair.into_inner() {
-            match inner_pair.as_rule() {
-                Rule::COMMENT => println!("Comment:  {}", inner_pair.as_str()),
-                Rule::expression => println!("Expression:   {}", inner_pair.as_str()),
-                Rule::functionCall => println!("Functioncall:   {}", inner_pair.as_str()),
-                Rule::EOI => println!("reached EOF"),
-                _ => unreachable!()
-            };
-        }
+fn direct_mode(input:&String,debug: bool) {
+    println!("will parse script as raw input");
+    let matches = parse(input);
+    if !debug {return}
+    for rule in matches {
+        match rule.as_rule() {
+            Rule::COMMENT => println!("Comment:  {}", rule.as_str()),
+            Rule::expression => println!("Expression:   {}", rule.as_str()),
+            Rule::functionCall => println!("Functioncall:   {}", rule.as_str()),
+            Rule::EOI => println!("reached EOF"),
+            _ => unreachable!()
+        };
     }
 }
 
-fn direct_mode(input:String) {
-    println!("will parse script as raw input");
-    parse(input);
-}
-
-fn file_mode(input:String){
+fn file_mode(input:&String,debug: bool){
+    println!("will parse script from file");
     let file = fs::read_to_string(&input).unwrap_or_else(|e| panic!("{}", e) );
-    println!("debug{:?}",file);
-    parse(file);
+    parse(&file);
 }
 
 fn arg_parser() {
-    let args: Vec<String> = env::args().collect();
-    let number_arguments: usize = args.len();
-    let mut counter: usize = 0;
-    let mut arguments:Vec<CmdArgument> = vec!();
+    let args: Vec<String> = env::args().collect();                              // collect imput from commandline
+    let mut counter: usize = 0;                                                 // counter for iterating over arguments
+    let mut arguments:Vec<Flag> = vec!();                                       // vector for storing parsed arguments
+    let mut debug = false;                                                // debug flag
+    
+    // iterate over arguments and parse them into the arguments vector
     for arg in &args {
-        if !(counter < number_arguments) {
+        // break if all arguments are parsed
+        if !(counter < args.len()) {
             break;
         }
-        if arg.starts_with('-') {
-            arguments.push(CmdArgument { flag: arg.to_string(), value: args[counter+1].to_string() })
+        // check if argument is a flag and if it is a valid flag
+        if arg.starts_with('-') && FLAGS.contains(&arg.as_str()) {
+            match arg.as_str(){
+                "-f" => arguments.push(Flag::F(args[counter+1].to_string())),
+                "-s" => arguments.push(Flag::S(args[counter+1].to_string())),
+                "-h" => arguments.push(Flag::H),
+                "-d" => arguments.push(Flag::D),
+                _ => println!("you should not be able to read this")
+            }
         }
         counter +=1;
     }
     println!("{:?}",arguments);
+
+    for argument in arguments {
+        match argument {
+            Flag::D => debug = true,
+            Flag::F(file) => file_mode(&file,debug),
+            Flag::S(script) => direct_mode(&script,debug),
+            Flag::H => println!("{}",HELPTEXT),
+        }
+    }
+
 }
 
 
 
+
+
 fn main() {
+
 
     // 1. call program as `lime <script>`
     arg_parser();
